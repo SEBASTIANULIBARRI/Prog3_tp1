842d3fd15d81977f4c7b33e2ecc24703cbbf49ac
SEBASTIANULIBARRI <sebastiannahuelulibarri@gmail.com>
2025-05-13 09:36:38 -0300

    Subida grafos

---
+++ Grafos/src/Arco.java
@@ -0,0 +1,48 @@
+import java.util.Objects;
+
+/*
+ * La clase arco representa un arco del grafo. Contiene un vertice origen, un vertice destino y una etiqueta.
+ * Nota: Para poder exponer los arcos fuera del grafo y que nadie los modifique se hizo esta clase inmutable
+ * (Inmutable: una vez creado el arco no es posible cambiarle los valores).
+ */
+public class Arco<T> implements Comparable<T>{
+
+	private int verticeOrigen;
+	private int verticeDestino;
+	private T etiqueta;
+
+	public Arco(int verticeOrigen, int verticeDestino, T etiqueta) {
+		this.verticeOrigen = verticeOrigen;
+		this.verticeDestino = verticeDestino;
+		this.etiqueta = etiqueta;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		Arco<?> arco = (Arco<?>) o;
+		return verticeOrigen == arco.verticeOrigen && verticeDestino == arco.verticeDestino;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(verticeOrigen, verticeDestino);
+	}
+
+	@Override
+
+	
+	public int getVerticeOrigen() {
+		return verticeOrigen;
+	}
+	
+	public int getVerticeDestino() {
+		return verticeDestino;
+	}
+
+	public T getEtiqueta() {
+		return etiqueta;
+	}
+
+}
---
+++ Grafos/src/GrafoDirigido.java
@@ -0,0 +1,111 @@
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+
+public class GrafoDirigido<T> implements Grafo<T> {
+
+	private HashMap<Integer, List<Arco<T>>> grafo = new HashMap<>();
+	@Override
+	public void agregarVertice(int verticeId) {
+		if(!contieneVertice(verticeId)){
+			grafo.put(verticeId,new ArrayList<>());
+		}
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void borrarVertice(int verticeId) {
+		if(contieneVertice(verticeId)){
+			 {
+				grafo.get(verticeId).clear();
+				grafo.remove(verticeId);
+			}
+			//Falta borrar las que entran pero necesito otros metodos.
+		}
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void agregarArco(int verticeId1, int verticeId2, T etiqueta) {
+		if (contieneVertice(verticeId1) && contieneVertice(verticeId2)){
+			Arco<T> a = new Arco<>(verticeId1,verticeId2,etiqueta);
+			grafo.get(verticeId1).add(a);
+		}
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void borrarArco(int verticeId1, int verticeId2) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean contieneVertice(int verticeId) {
+		// TODO Auto-generated method stub
+		return grafo.containsKey(verticeId);
+	}
+
+	@Override
+	public boolean existeArco(int verticeId1, int verticeId2) {
+		// TODO Auto-generated method stub
+		List<Arco<T>> adyacentes = grafo.get(verticeId1);
+		return adyacentes != null && adyacentes.contains(new Arco<Integer>(verticeId1,verticeId2,0));
+	}
+
+	@Override
+	public Arco<T> obtenerArco(int verticeId1, int verticeId2) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int cantidadVertices() {
+		// TODO Auto-generated method stub
+		return grafo.keySet().size();
+	}
+
+	@Override
+	public int cantidadArcos() {
+		// TODO Auto-generated method stub
+		return arcos.size();
+	}
+
+	@Override
+	public Iterator<Integer> obtenerVertices() {
+		// TODO Auto-generated method stub
+		return grafo.keySet().iterator();
+	}
+
+	@Override
+	public Iterator<Integer> obtenerAdyacentes(int verticeId) {
+		if(contieneVertice(verticeId))
+			return grafo.get(verticeId).iterator();
+		return null;
+	}
+
+	@Override
+	public Iterator<Arco<T>> obtenerArcos() {
+		// TODO Auto-generated method stub
+		List<Arco<T>> todosLosArcos = new ArrayList<>();
+
+		for (Integer i : grafo.keySet()) {
+			Iterator<Arco<T>> it = obtenerArcos(i); // este es tu método por vértice
+			while (it.hasNext()) {
+				todosLosArcos.add(it.next());
+			}
+		}
+
+		return todosLosArcos.iterator();
+	}
+
+	@Override
+	public Iterator<Arco<T>> obtenerArcos(int verticeId) {
+		List<Arco<T>> arcos = grafo.get(verticeId);
+		// TODO Auto-generated method stub
+		return (arcos == null) ? null : arcos.iterator();
+	}
+
+}
